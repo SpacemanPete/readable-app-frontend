5d62b0a11f73d986930b7d0996684f0193e65b87
Piotr Butkiewicz <piotrab@gmail.com>
2018-01-03 22:27:20 -0500

    initial landing page stlying

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import logo from '../logo.png'
 import '../styles/App.css'
+import { Route, Link } from 'react-router-dom'
 
 class App extends Component {
   render() {
@@ -10,10 +11,30 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Readable App for Udacity</h1>
         </header>
-        <nav></nav>
-        <aside></aside>
-        <section></section>
-        <footer></footer>
+        <nav id="main-menu" >
+          <Link to="/" className="font_large" >Home</Link>
+          <Link to="add-post" className="font_large" >Add new post</Link>
+        </nav>
+        <aside id="categories" >
+          <ul>
+            <li>Category</li>
+            <li>Category</li>
+            <li>Category</li>
+            <li>Category</li>
+            <li>Category</li>
+          </ul>
+        </aside>
+        <section id="content">
+          <Route exact path="/" render={() => (
+            <p>Here we render all the post content</p>
+          )} />
+          <Route path="/add-post" render={() => (
+            <p>This is the Add Post dialoge</p>
+          )} />
+        </section>
+        <footer>
+          <div className="attribution">Built by <a href='https://github.com/SpacemanPete'>SpacemanDev</a> for Udacity's React Certificate Course</div>
+        </footer>
       </div>
     )
   }
