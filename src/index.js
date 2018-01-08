05d6c29cb24307458b91a08ffac64da24e6ddd79
Piotr Butkiewicz <piotrab@gmail.com>
2018-01-07 19:46:13 -0500

    wire Provider with store to main App component

--- src/index.js
+++ src/index.js
@@ -5,9 +5,20 @@ import './styles/index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import configureStore from '../configureStore'
+
+
+
+const store = configureStore()
+
+console.log(store.getState())
+
 
 ReactDOM.render(
-  <BrowserRouter><App /></BrowserRouter>, 
+  <Provider store={store}>
+    <BrowserRouter><App /></BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 )
 registerServiceWorker()
