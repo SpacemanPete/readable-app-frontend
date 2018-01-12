2d4cf7aede0c39e7d5560953d1b7417cd5e32f8b
Piotr Butkiewicz <piotrab@gmail.com>
2018-01-12 01:19:41 -0500

    begin configuring reducers file to handle category get all action

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,21 +1,50 @@
 import { combineReducers } from 'redux'
 import { 
-  ADD_POST,
-  EDIT_POST,
-  DELETE_POST,
-  POST_VOTE_UP,
-  POST_VOTE_DOWN,
-  ADD_COMMENT,
-  EDIT_COMMENT,
-  DELETE_COMMENT 
+  GET_ALL_CATEGORIES,
+  // GET_POSTS_BY_CATEGORY,
+  // GET_COMMENTS_FOR_POST,
+  // ADD_POST,
+  // EDIT_POST,
+  // DELETE_POST,
+  // POST_VOTE_UP,
+  // POST_VOTE_DOWN,
+  // ADD_COMMENT,
+  // EDIT_COMMENT,
+  // DELETE_COMMENT 
 } from '../actions'
 
-function postsByCategory( state = {}, action ) {
-
+function categories( state = {}, action ) {
+  switch (action.type) {
+  case GET_ALL_CATEGORIES :
+    return state
+  default :
+    return state
+  }
 }
 
+// function posts( state = {}, action ) {
+//   switch (action.type) {
+//   case GET_POSTS_BY_CATEGORY :
+//     return state
+//   default :
+//     return state
+//   }
+// }
+
+// function comments( state = {}, action ) {
+//   switch (action.type) {
+//   case GET_COMMENTS_FOR_POST :
+//     return state
+//   default :
+//     return state
+//   }
+// }
+
+
 const rootReducer = combineReducers({
-  postsByCategory
+  categories,
+  // posts,
+  // comments
 })
 
 export default rootReducer
