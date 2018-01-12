e422f5b271fb660982e19da872a537c8332c5aec
Piotr Butkiewicz <piotrab@gmail.com>
2018-01-12 01:19:12 -0500

    begin creating api file with handlers for the API endpoints

---
+++ src/utils/api.js
@@ -0,0 +1,41 @@
+const api = process.env.REACT_APP_READABLE_API_URL || 'http://localhost:3001'
+
+let token = localStorage.token
+
+if (!token)
+  token = localStorage.token = Math.random().toString(36).substr(-8)
+
+const headers = {
+  'Accept': 'application/json',
+  'Authorization': token
+}
+
+// Functions below are from example contacts API form another project
+// TODO: Wire up the connector functions to talk to server API and
+// wire into actions/reducers functionality
+
+
+export const getAllCategories = () =>
+  fetch(`${api}/categories`, { headers })
+    .then(res => res.json())
+    // .then(data => data.categories)
+
+// export const getAll = () =>
+//   fetch(`${api}/contacts`, { headers })
+//     .then(res => res.json())
+//     .then(data => data.contacts)
+
+// export const remove = (contact) =>
+//   fetch(`${api}/contacts/${contact.id}`, { method: 'DELETE', headers })
+//     .then(res => res.json())
+//     .then(data => data.contact)
+
+// export const create = (body) =>
+//   fetch(`${api}/contacts`, {
+//     method: 'POST',
+//     headers: {
+//       ...headers,
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(body)
+//   }).then(res => res.json())
