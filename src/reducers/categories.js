c5da9248c772dfd3e357058cf2b72d8e87b54e27
Piotr Butkiewicz <piotrab@gmail.com>
2018-01-15 13:02:50 -0500

    refactor categories reducer to output empty array as initial state as well as correctly pass data from server response

--- src/reducers/categories.js
+++ src/reducers/categories.js
@@ -2,20 +2,11 @@ import {
   RECEIVE_CATEGORIES,
 } from '../actions'
 
-export default function categories( state = {}, action ) {
-  const { categories } = action
-
+export default function categories( categories = [], action ) {
   switch (action.type) {
   case RECEIVE_CATEGORIES :
-    // This spread operator makes for the most baffling syntax. 
-    // Try as I might I have such a hard time grokking.
-    // I threw spaghetti at the wall for hours getting this to work
-    // I read it now and I still can't describe it in English, sigh...
-    return {
-      ...state,
-      ...categories
-    }
+    return action.categories
   default :
-    return state
+    return categories
   }
 }
